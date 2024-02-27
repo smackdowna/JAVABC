381ad32767f0934e4aaa95108e15cd581dc50237
Rishi <rishiraj1096@gmail.com>
2024-02-27 12:41:26 +0530

    revert changes

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -9,6 +9,28 @@ const instance = new Razorpay({
   key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
+////stripe------------------------------
+exports.processPayment = catchAsyncErrors(async (req, res, next) => {
+  const { amount } = req.body;
+
+  if (!amount) return next(new ErrorHandler("Please enter  amount", 400));
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: Number(amount) * 100,
+    currency: "inr",
+  });
+
+  return res.status(201).json({
+    success: true,
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+});
+
+
 
 ////---------------------Razorpay------------------------------------------------
 
