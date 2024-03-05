84a72ba64d81eec0f8ad6ba6be21e3344fb62461
Rishi <rishiraj1096@gmail.com>
2024-03-05 09:19:02 +0530

    message

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
 
   if (!token) {
-    return next(new ErrorHandler("Please Login to access this resource", 401));
+    return next(new ErrorHandler("Please Login", 401));
   }
 
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
